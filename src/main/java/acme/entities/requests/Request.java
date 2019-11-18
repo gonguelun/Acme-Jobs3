bb76a8671ba3ca2970840ccc12c2624d626f9923
Mario David <marjimhar@alum.us.es>
2019-11-18 19:06:25 +0100

    Task 029: finished

--- src/main/java/acme/entities/requests/Request.java
+++ src/main/java/acme/entities/requests/Request.java
@@ -24,7 +24,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(indexes = {
-	@Index(columnList = "deadline, ticker")
+	@Index(columnList = "deadline"), @Index(columnList = "ticker"), @Index(columnList = "reward")
 })
 
 public class Request extends DomainEntity {
